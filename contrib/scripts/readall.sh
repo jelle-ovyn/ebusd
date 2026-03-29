79371ce246e4b80fb574e054272d1bd494a5d5ec
John <ebusd@ebusd.eu>
2026-03-29 11:37:35 +0200

    enhance util scripts

--- contrib/scripts/readall.sh
+++ contrib/scripts/readall.sh
@@ -1,22 +1,102 @@
-#!/bin/sh
+#!/bin/bash
+
+# print help
+help() {
+  cat << EOF
+Usage: $0 [OPTION...] [FIND-ARGS...]
+Helper script to read all known/matching messages from an ebusd instance.
+
+ Options:
+  -s, --server=HOST   Connect to ebusd on HOST (name or IP) [localhost]
+  -p, --port=PORT     Connect to ebusd on PORT [8888]
+  -t, --timeout=SECS  Timeout for connecting to/receiving from ebusd, 0 for
+                      none [60]
+  -R, --readargs=...  Additional arguments to pass to the 'read' command []
+
+If given, FIND-ARGS can be used to pass arguments to the 'find' command to
+limit/widen the message list, e.g. '-c hwc' for the 'hwc' circuit.
+EOF
+}
+
+# parse cmdline options
+OPTS=$(getopt -n ebusctl -o 's:p:t:R:h' -l 'server:,port:,timeout:,readargs:,help' -- "$@")
+if [ $? -ne 0 ]; then
+  help
+  exit 1
+fi
+eval set -- "$OPTS"
+server=localhost
 port=8888
-if [ "x$1" = "x-p" ]; then
-  shift
-  port=$1
-  shift
-fi
+timeout=60
 readargs=
-if [ "x$1" = "x-R" ]; then
-  shift
-  readargs=$1
-  shift
+while true; do
+  case "$1" in
+    '-s'|'--server')
+      shift
+      server="$1"
+      shift
+      if [ -z "$server" ]; then
+        echo "invalid server" >&2
+        exit 1
+      fi
+      continue
+      ;;
+    '-p'|'--port')
+      shift
+      port="$1"
+      shift
+      if [ "$port" -lt 1 ] || [ "$port" -gt 65535 ]; then
+        echo "invalid port" >&2
+        exit 1
+      fi
+      continue
+      ;;
+    '-t'|'--timeout')
+      shift
+      timeout="$1"
+      shift
+      if [ "$timeout" -lt 0 ] || [ "$timeout" -gt 3600 ]; then
+        echo "invalid timeout" >&2
+        exit 1
+      fi
+      continue
+      ;;
+    '-R'|'--readargs')
+      shift
+      readargs="$1"
+      shift
+      continue
+      ;;
+    '-h'|'--help')
+      shift
+      help
+      exit
+      ;;
+    '--')
+      shift
+      break
+      ;;
+    *)
+      echo "error" >&2
+      exit 1
+      ;;
+  esac
+done
+
+# set arguments to netcat
+args=(-q 0)
+if [ "$timeout" -gt 0 ]; then
+  args+=(-w "$timeout")
 fi
-for i in `echo "f -F circuit,name" "$@"|nc -q 1 127.0.0.1 $port|sort -u|grep ','`; do
+args+=("$server" "$port")
+
+# query the messages
+for i in `echo "f -F circuit,name" "$@"|nc "${args[@]}"|sort -u|grep ','`; do
   circuit=${i%%,*}
   name=${i##*,}
   if [ -z "$circuit" ] || [ -z "$name" ] || [ "$circuit,$name" = "scan,id" ]; then
     continue
   fi
-  ret=`echo "r ${readargs} -c $circuit $name" |nc -q 1 127.0.0.1 $port|head -n 1`
+  ret=`echo "r ${readargs} -c $circuit $name" |nc "${args[@]}"|head -n 1`
   echo "$circuit $name = $ret"
 done
--- contrib/scripts/readallvaillantregisters.sh
+++ contrib/scripts/readallvaillantregisters.sh
@@ -1,18 +1,133 @@
 #!/bin/bash
+# readallvaillantregisters.sh - helper script to read all Vaillant registers from an ebusd instance.
+
+# print help
+help() {
+  cat << EOF
+Usage: $0 [OPTION...]
+Helper script to read all Vaillant registers from an ebusd instance.
+
+ Options:
+  -s, --server=HOST   Connect to ebusd on HOST (name or IP) [localhost]
+  -p, --port=PORT     Connect to ebusd on PORT [8888]
+  -t, --timeout=SECS  Timeout for connecting to/receiving from ebusd, 0 for
+                      none [60]
+  -a, --addr=ZZ       Address to read (hex) [08]
+  -f, --from=NUM      First register to read [0]
+  -c, --count=NUM     Number of registers to read [128]
+EOF
+}
+
+# parse cmdline options
+OPTS=$(getopt -n ebusctl -o 's:p:t:a:f:c:h' -l 'server:,port:,timeout:,addr:,from:,count:,help' -- "$@")
+if [ $? -ne 0 ]; then
+  help
+  exit 1
+fi
+eval set -- "$OPTS"
+server=localhost
 port=8888
-if [ "x$1" = "x-p" ]; then
-  shift
-  port=$1
-  shift
-fi
+timeout=60
 addr=08
-if [ "x$1" = "x-a" ]; then
-  shift
-  addr=$1
-  shift
+from=0
+count=128
+while true; do
+  case "$1" in
+    '-s'|'--server')
+      shift
+      server="$1"
+      shift
+      if [ -z "$server" ]; then
+        echo "invalid server" >&2
+        exit 1
+      fi
+      continue
+      ;;
+    '-p'|'--port')
+      shift
+      port="$1"
+      shift
+      if [ "$port" -lt 1 ] || [ "$port" -gt 65535 ]; then
+        echo "invalid port" >&2
+        exit 1
+      fi
+      continue
+      ;;
+    '-t'|'--timeout')
+      shift
+      timeout="$1"
+      shift
+      if [ "$timeout" -lt 0 ] || [ "$timeout" -gt 3600 ]; then
+        echo "invalid timeout" >&2
+        exit 1
+      fi
+      continue
+      ;;
+    '-a'|'--addr')
+      shift
+      addr="$1"
+      shift
+      if [ "${#addr}" != 2 ]; then
+        echo "invalid addr" >&2
+        exit 1
+      fi
+      continue
+      ;;
+    '-f'|'--from')
+      shift
+      from="$1"
+      shift
+      if [ "$from" -lt 0 ] || [ "$from" -gt 65535 ]; then
+        echo "invalid from" >&2
+        exit 1
+      fi
+      from=$(( $from ))
+      continue
+      ;;
+    '-c'|'--count')
+      shift
+      count="$1"
+      shift
+      if [ "$count" -lt 1 ] || [ "$count" -gt 65535 ]; then
+        echo "invalid count" >&2
+        exit 1
+      fi
+      count=$(( $count ))
+      continue
+      ;;
+    '-h'|'--help')
+      shift
+      help
+      exit
+      ;;
+    '--')
+      shift
+      break
+      ;;
+    *)
+      echo "error" >&2
+      exit 1
+      ;;
+  esac
+done
+
+# check from/to bounds
+to=$(( $from + $count ))
+if [ "$to" -gt 65535 ]; then
+  echo "invalid from/count" >&2
+  exit 1
 fi
-for (( i=0; i<512; i++ )) ; do
+
+# set arguments to netcat
+args=(-q 0)
+if [ "$timeout" -gt 0 ]; then
+  args+=(-w "$timeout")
+fi
+args+=("$server" "$port")
+
+# query the registers
+for (( i=$from; i<$to; i++ )) ; do
   h=`printf "%4.4X" $i`
-  ret=`echo "hex ${addr}b509030d${h##??}${h%%??}"|nc -q 1 localhost $port|head -n 1`
+  ret=`echo "hex ${addr}b509030d${h##??}${h%%??}"|nc "${args[@]}"|head -n 1`
   echo $i "=" $ret
 done
